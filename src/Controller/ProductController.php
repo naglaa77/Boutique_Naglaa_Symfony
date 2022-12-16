448337b99b01166ab09fedcd0108a043873e2573
naglaa77 <sama_hap@yahoo.com>
2022-12-16 21:56:03 +0100

    finish show page fro single prodcu

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -29,4 +29,21 @@ class ProductController extends AbstractController
         
         ]);
     }
+
+
+    #[Route('/produit/{slug}', name: 'product')]
+    public function show($slug): Response
+    {
+
+        $product = $this->entityManger->getRepository(Product::class)->findOneBySlug($slug);
+        
+        if(!$product) {
+            return $this->redirectToRoute('products');
+
+        }
+        return $this->render('product/show.html.twig',[
+            'product' => $product
+        
+        ]);
+    }
 }
